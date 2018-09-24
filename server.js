75b37451abe6f7140c6d690c73e0ec546e7b33d4
Alex Borodin <hager@yandex.ru>
2018-09-24 17:46:34 +0300

    some other lint fixes

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.use(morgan(':remote-addr [:date[clf]] :method :url :status :res[content-leng
 app.use('/public', express.static('./public'));
 
 // Serve the files on port 3000.
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!\n');
 });
